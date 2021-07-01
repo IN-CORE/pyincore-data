b82180c38e8d2fb6f6c2b44ed37eed1c999a3343
Yong Wook Kim <ywkim@illinois.edu>
2021-07-01 16:30:23 -0500

    added pytest and format test

--- pyincore_data/globals.py
+++ pyincore_data/globals.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 University of Illinois and others. All rights reserved.
+# Copyright (c) 2021 University of Illinois and others. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
@@ -8,13 +8,10 @@ import os
 import logging
 from logging import config as logging_config
 
-PACKAGE_VERSION = "1.2.0"
+PACKAGE_VERSION = "0.1.0"
 
-INCORE_GEOSERVER_WMS_URL = "https://incore.ncsa.illinois.edu/geoserver/incore/wms"
-INCORE_GEOSERVER_DEV_WMS_URL = "https://incore-dev.ncsa.illinois.edu/geoserver/incore/wms"
-
-PYINCORE_VIZ_ROOT_FOLDER = os.path.dirname(os.path.dirname(__file__))
+PYINCORE_DATA_ROOT_FOLDER = os.path.dirname(os.path.dirname(__file__))
 
 LOGGING_CONFIG = os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'logging.ini'))
 logging_config.fileConfig(LOGGING_CONFIG)
-LOGGER = logging.getLogger('pyincore-viz')
+LOGGER = logging.getLogger('pyincore-data')
---
+++ tests/pyincore_data/test_pyincore_data.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 University of Illinois and others. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License v2.0 which accompanies this distribution,
+# and is available at https://www.mozilla.org/en-US/MPL/2.0/
+
+import pytest
+
+from pyincore_data.censusutil import CensusUtil
+
+
+@pytest.fixture
+def client():
+    return pytest.client
+
+
+def test_get_blockgroupdata_for_dislocation():
+    state_counties = ['01001', '01003']
+    disloc_df = CensusUtil.get_blockgroupdata_for_dislocation(state_counties)
+
+    assert disloc_df['Survey'][0] == '2010 dec/sf1'
