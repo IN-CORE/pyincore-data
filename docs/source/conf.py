8f185d995d2b3e80e6b480abc03f2f5c2c3833ad
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2021-10-28 09:51:09 -0500

    release 0.3.0 (#14) (#16)
    * modified release script
    
    * added github action (#8)
    
    * added github action
    
    * modified the version in master branch
    
    * added geopackage data output (#10)
    
    * added conda recipe (#6)
    
    * added conda recipe
    
    * modified python version
    
    * Added unit test (#12)
    
    * Added unit test
    
    * added numpy to env
    
    * added extra options
    
    * added clear cache
    
    * retrieved docker build
    
    * separated pytest and docker build
    
    * release 0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ project = 'pyIncore-data'
 author = 'Yong Wook Kim'
 
 # The short X.Y version
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
