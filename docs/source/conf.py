e4ed311447d64d52d98bc898c33e9f7add61ae01
YONG WOOK KIM <ywkim@illinois.edu>
2022-03-31 15:35:44 -0500

    fixed readme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../pyincore_data'))
-sys.path.insert(0, os.path.abspath('../../tests'))
+# sys.path.insert(0, os.path.abspath('../../tests'))
 
 # -- Project information -----------------------------------------------------
 
