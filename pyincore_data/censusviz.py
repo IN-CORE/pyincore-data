4969a7fff9c5497c0a3b9d3198665c58dd10130c
YONG WOOK KIM <ywkim@illinois.edu>
2022-04-15 15:13:03 -0500

    refactoring methods

--- pyincore_data/censusviz.py
+++ pyincore_data/censusviz.py
@@ -21,60 +21,48 @@ class CensusViz():
     """Utility methods for Census data and visualization"""
 
     @staticmethod
-    def create_dislocation_ipyleaflet_map_from_gpd(in_gpd, zoom_level=10):
-        """Create ipyleaflet dislocation map for geodataframe.
+    def create_choropleth_layer(geo_data, choro_data, name):
+        """Create ipyleaflet layer for choropleth map.
 
         Args:
-            in_gpd (object): Geodataframe of the dislocation.
+            geo_data (object): ditionary from geodataframe
+            choro_data (object): choropleth data from geodataframe
+            name (str): name for the layer
+
+        Returns:
+            obj : An ipyleaflet layer
+
+        """
+        layer = ipylft.Choropleth(
+            geo_data=geo_data,
+            choro_data=choro_data,
+            colormap=linear.YlOrRd_04,
+            border_color='black',
+            style={'fillOpacity': 0.8},
+            name=name
+        )
+
+        return layer
+
+    @staticmethod
+    def create_ipyleafletmap_from_geodataframe(gpd, zoom_level=100):
+        """Create base ipyleaflet map from geodataframe.
+
+        Args:
+            gpd (object): Geodataframe of the dislocation.
+            zoom_level (int): default zoom level for the map
 
         Returns:
             obj : An ipyleaflet map for dislocation
 
         """
-        # What location should the map be centered on?
-        center_x = in_gpd.bounds.minx.mean()
-        center_y = in_gpd.bounds.miny.mean()
+        center_x = gpd.bounds.minx.mean()
+        center_y = gpd.bounds.miny.mean()
 
         out_map = ipylft.Map(center=(center_y, center_x), zoom=zoom_level,
                              crs=projections.EPSG3857, scroll_wheel_zoom=True)
 
-        in_gpd_tmp = in_gpd[['GEOID10', 'phispbg', 'pblackbg', 'geometry']]
-        geo_data_dic = json.loads(in_gpd_tmp.to_json())
-        hisp_choro_data = CensusViz.create_choro_data_from_pd(in_gpd, 'phispbg')
-        black_choro_data = CensusViz.create_choro_data_from_pd(in_gpd, 'pblackbg')
-
-        # Add Percent Hispanic to Map
-        layer1 = ipylft.Choropleth(
-            geo_data=geo_data_dic,
-            choro_data=hisp_choro_data,
-            colormap=linear.YlOrRd_04,
-            border_color='black',
-            style={'fillOpacity': 0.8},
-            name='phispbg'
-        )
-
-        # Add Percent Black to Map
-        layer2 = ipylft.Choropleth(
-            geo_data=geo_data_dic,
-            choro_data=black_choro_data,
-            colormap=linear.YlOrRd_04,
-            border_color='black',
-            style={'fillOpacity': 0.8},
-            name='pblackbg'
-        )
-
-        out_map.add_layer(layer1)
-        out_map.add_layer(layer2)
-
-        out_map.add_control(ipylft.LayersControl(position='topright'))
-        out_map.add_control(ipylft.FullScreenControl(position='topright'))
-
-        # return geodataframe and map
-        bgmap = {}  # start an empty dictionary
-        bgmap['gdf'] = in_gpd.copy()
-        bgmap['map'] = out_map
-
-        return bgmap
+        return out_map
 
     @staticmethod
     def create_choro_data_from_pd(pd, key):
@@ -161,3 +149,44 @@ class CensusViz():
         map_save_file = programname+'/'+savefile+'_map.html'
         print('Dynamic HTML map saved to: '+map_save_file)
         folium_map.save(map_save_file)
+
+    @staticmethod
+    def create_dislocation_ipyleaflet_map_from_gpd(in_gpd, zoom_level=10):
+        """Create ipyleaflet dislocation map for geodataframe.
+
+        Args:
+            in_gpd (object): Geodataframe of the dislocation.
+            zoom_level (int): default zoom level for the map
+
+        Returns:
+            obj : An ipyleaflet map for dislocation
+
+        """
+        out_map = CensusViz.create_ipyleafletmap_from_geodataframe(in_gpd, zoom_level)
+
+        # skim only the necessary field from the geodataframe
+        in_gpd_tmp = in_gpd[['GEOID10', 'phispbg', 'pblackbg', 'geometry']]
+        geo_data_dic = json.loads(in_gpd_tmp.to_json())
+        hisp_choro_data = CensusViz.create_choro_data_from_pd(in_gpd, 'phispbg')
+        black_choro_data = CensusViz.create_choro_data_from_pd(in_gpd, 'pblackbg')
+
+        # Add Percent Hispanic to Map
+        layer1 = CensusViz.create_choropleth_layer(
+            geo_data_dic, hisp_choro_data, "phispbg")
+
+        # Add Percent Black to Map
+        layer2 = CensusViz.create_choropleth_layer(
+            geo_data_dic, black_choro_data, "pblackbg")
+
+        out_map.add_layer(layer1)
+        out_map.add_layer(layer2)
+
+        out_map.add_control(ipylft.LayersControl(position='topright'))
+        out_map.add_control(ipylft.FullScreenControl(position='topright'))
+
+        # return geodataframe and map
+        bgmap = {}  # start an empty dictionary
+        bgmap['gdf'] = in_gpd.copy()
+        bgmap['map'] = out_map
+
+        return bgmap
