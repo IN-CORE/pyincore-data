e6084e9b3ebbce3147140696211df6d18c2512ce
YONG WOOK KIM <ywkim@illinois.edu>
2022-06-27 12:39:22 -0500

    release 0.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ project = 'pyIncore-data'
 author = 'Yong Wook Kim'
 
 # The short X.Y version
-version = '0.4'
+version = '0.5'
 # The full version, including alpha/beta/rc tags
-release = '0.4.1'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
